163869fc94ca09b954b7732d004cf7cf88ce926e
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2019-09-01 22:09:57 +1000

    Add more homepage tests

--- cypress/integration/Online-Portfolio/home.js
+++ cypress/integration/Online-Portfolio/home.js
@@ -10,4 +10,20 @@ describe('Portfolio cypress test on the home page', () => {
         cy.get('@title').contains('About Me')
         cy.get('@title').should('be.visible')
     })
+
+    it('Check that the Resume link is correct', () => {
+        cy.get('#Resume').should('be.visible')
+        cy.get('#Resume').should('have.attr', 'href').and('include', 'https://drive.google.com')
+    })
+
+    it('Check that the Work link is going to the Work webpage', () => {
+        cy.get('#Worklink').should('be.visible')
+        cy.get('#Worklink').click()
+        cy.url().should('include', '/work')
+    })
+
+    it('Check that the link to the Github repo is correct', () => {
+        cy.get('#Portfolio').should('be.visible')
+        cy.get('#Portfolio').should('have.attr', 'href').and('include', 'https://github.com/Dhaigh94/Online-Portfolio')
+    })
 })
