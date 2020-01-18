a9726f715385ba339f2e3b3d780223ca01201bec
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2020-01-18 18:42:19 +1100

    Rename file for Junior tester blog

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get("/my-testing-story", function(req, res){
 
 // WHAT A PRODUCT OWNER WANTS FROM A JUNIOR TESTER ROUTE
 app.get("/what-a-product-owner-wants-from-a-junior-tester", function(req, res){
-    res.render("blogpages/WAPOWFAJT");
+    res.render("blogpages/juniortester");
 });
 
 // RESOURCES ROUTE
