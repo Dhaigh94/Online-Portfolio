cb7967cb7ba8e976a51c4321e8b650f921620264
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2020-04-16 17:46:45 +1000

    Add Blog Tests (#45)

---
+++ cypress/integration/Online-Portfolio/blog.js
@@ -0,0 +1,27 @@
+describe('Portfolio Blog tests', () => {
+    beforeEach(function() {
+        cy.visit('/')
+        cy.url().should('include', 'http://localhost:8080')
+        cy.title().should('eq', "David's Portfolio")
+        cy.get('[data-cy=Blog]').click()
+        cy.url().should('include', '/blog')
+    })
+
+    it('Check that the user can go to the What a Product Owner Wants from a Junior Tester blog page', function() {
+        cy.get('[data-cy=Product]').click()
+        cy.url().should('include', '/what-a-product-owner-wants-from-a-junior-tester')
+        cy.get('h1').as('title')
+        cy.get('@title').contains('What a Product Owner Wants from a Junior Tester')
+        cy.get('@title').should('be.visible')
+        cy.percySnapshot('What a Product Owner Wants from a Junior Tester')
+    })
+
+    it('Check that the user can go to the My Testing Story blog page', function() {
+        cy.get('[data-cy=Story]').click()
+        cy.url().should('include', '/my-testing-story')
+        cy.get('h1').as('title')
+        cy.get('@title').contains('My Testing Story')
+        cy.get('@title').should('be.visible')
+        cy.percySnapshot('My Testing Story')
+    })
+})
